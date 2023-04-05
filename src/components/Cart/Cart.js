6cf534ace74fd1acf62078ab433bcf4693b23ca0
eshwagadkar <eshwagadkar007@gmail.com>
2023-04-05 08:42:35 +0530

    Modal Overlay using React Portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = props => {
@@ -6,17 +7,20 @@ const Cart = props => {
         [{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 },].map((item)=> <li>{item.name}</li>)
         }</ul>
 
-    return <div>
+    return (
+        <Modal>
         {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>35.89</span>
-        </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
-            <button className={classes.button}>Order</button>
-        </div>
-    </div>
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.89</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+      </Modal>
+    );
+       
 };
 
 export default Cart;
